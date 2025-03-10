cf54280080c98c9086037a7ed881c8abe2395a9d
mr.panahov <panahovemin10@gmail.com>
2025-03-10 17:43:13 +0400

    Last Changes

--- PDFProcessorService/src/main/java/com/example/pdfprocessorservice/client/AIClient.java
+++ PDFProcessorService/src/main/java/com/example/pdfprocessorservice/client/AIClient.java
@@ -3,7 +3,7 @@ package com.example.pdfprocessorservice.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "ai-service", url = "${ai.service.url}")
+@FeignClient(name = "AIService", url = "${ai.service.url}")
 public interface AIClient {
 
     @PostMapping("/analyze")
